10c80ff928cf60531776e84e6274e9cfc2261041
sakamotodd <skmt_dgr@yahoo.co.jp>
2022-06-11 15:31:19 +0900

    37-faviconの表示方法

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const PORT = process.env.PORT;
 const path = require("path");
 const express = require("express");
+const favicon = require("serve-favicon");
 const app = express();
 
 // Express Setttings
 app.set("view engine", "ejs");
 
 // Static resource rooting
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // Dynamic resource rooting
@@ -16,3 +18,4 @@ app.use("/", require("./routes/index.js"));
 app.listen(PORT, () => {
   console.log(`Application listening at ${PORT}`);
 });
+   
